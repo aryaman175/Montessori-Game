9d157929f72d173da822693a92fc9f83eb0a87a5
aryaman175 <64005489+aryaman175@users.noreply.github.com>
2021-02-03 08:44:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+  // opening tabs on clicking images
+  $("#1").on("click",function()
+   {
+     window.open("https://aryaman175.github.io/Food/", '_blank');
+   });
+  $("#2").on("click",function()
+  {
+      window.open("https://aryaman175.github.io/Transport/", '_blank');
+  });
+  $("#3").on("click",function()
+   {
+     window.open("https://aryaman175.github.io/Fruits/", '_blank');
+   });
+});
